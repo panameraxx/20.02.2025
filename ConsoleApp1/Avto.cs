944fd520c7518a705b7dccc6833975e033961459
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-02-20 10:59:27 +0200

    Add files via upload

---
+++ ConsoleApp1/Avto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Avto
+    {
+        private string brand;
+        private string color;
+        private double skor;
+
+        public double Skor
+        {
+            get { return skor; }
+            set
+            {
+                if (value < 20) skor = 20;
+                else if (skor > 120) skor = 120;
+                else skor = value;
+            }
+        }
+
+        public string Brand
+        {
+            get { return brand; }
+            set
+            { brand = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set
+            { color = value; }
+        }
+        public Avto() { }
+        public Avto(string brand)
+        {
+            this.brand = brand;
+        }
+        public Avto(string brand,string color, double skor) :this(color)
+        {
+            this.color = color;
+            this.skor = skor;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Бренд: {Brand}\t Цвет: {Color}\t Скорость: {skor}");
+        }
+        public string Tostring()
+        {
+            return $"Бренд: {Brand}\t Цвет: {Color}\t Скорость: {skor}";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto = new Avto();
+            avto.Brand = "Мерседес";
+            avto.Color = "Зеленый";
+            avto.Skor = 30;
+            avto.ShowInfo();
+            Console.WriteLine("Введите бренд:");
+            string brend = Console.ReadLine();
+            Console.WriteLine("Введите цвет:");
+            string cvet = Console.ReadLine();
+            int skor = int.Parse(Console.ReadLine());
+            Avto avto1 = new Avto(brend,cvet,skor);
+            Console.WriteLine($"{avto1.Tostring()}");
+            Console.ReadKey();
+        }
+    }
+}
